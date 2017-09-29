a35f7b898f0f2bd8218d3a911f1fbfeb1bc49c1c
Rushi Sangani <rushis@meditab.com>
2017-09-29 21:05:30 +0530

    Initial Commit

---
+++ RSSelectionMenu/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RSSelectionMenu
+//
+//  Created by Rushi on 29/09/17.
+//  Copyright © 2017 Rushi Sangani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
