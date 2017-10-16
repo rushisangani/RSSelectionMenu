cf2f8a56753d97072cf1b64812ed46a7039a6b23
Rushi Sangani <rushisangani@gmail.com>
2017-10-17 00:09:29 +0530

    New version updated to 2.0.5

---
+++ RSSelectionMenu/build/RSSelectionMenu.build/Debug-iphoneos/RSSelectionMenu.build/DerivedSources/RSSelectionMenu_vers.c
@@ -0,0 +1,5 @@
+ extern const unsigned char RSSelectionMenuVersionString[];
+ extern const double RSSelectionMenuVersionNumber;
+
+ const unsigned char RSSelectionMenuVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:RSSelectionMenu  PROJECT:RSSelectionMenu-1" "\n";
+ const double RSSelectionMenuVersionNumber __attribute__ ((used)) = (double)1.;
---
+++ RSSelectionMenu/build/RSSelectionMenu.build/Debug-iphoneos/RSSelectionMenu.build/Objects-normal/armv7/RSSelectionMenu_vers.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/rushisangani/Downloads/RSSelectionMenu-1.0.5/RSSelectionMenu/build/RSSelectionMenu.build/Debug-iphoneos/RSSelectionMenu.build/DerivedSources/RSSelectionMenu_vers.c
